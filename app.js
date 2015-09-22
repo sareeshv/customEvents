ed8b7040fe69af73445d8f8242a5bb13adf2772e
SAREESH <vsareesh@gmail.com>
2015-09-22 14:35:37 -0700

    app.js modified

--- app.js
+++ app.js
@@ -3,5 +3,29 @@
  */
 (function(){
 
+	//var SampleObject = {
+	//	WindowHeightReady : $.Deferred()
+	//};
+
+	/**
+	 * sample function
+	 */
+	var sampleFunction = function() {
+		console.log('My sample function to trigger on dispatching a custom event');
+	};
+
+	//Used timeout to show the behaviour
+	setTimeout(function () {
+		document.dispatchEvent(new Event("windowHeightReady"));
+	}, 5000);
+
+
+
+
+	//document.addEventListener('windowHeightReady', function(e) {
+	//	SampleObject.WindowHeightReady.resolve();
+	//}, false);
+
+	document.addEventListener('windowHeightReady', sampleFunction);
 
 })();
